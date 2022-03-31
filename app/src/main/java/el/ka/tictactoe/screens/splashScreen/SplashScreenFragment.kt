afc332a013a28ba3fc4ed2e42a441a08b5fd2582
Ekaterina-El <hackerrussia@yandex.ru>
2022-03-31 20:18:46 +0500

    Added functional for splash screen

--- app/src/main/java/el/ka/tictactoe/screens/splashScreen/SplashScreenFragment.kt
+++ app/src/main/java/el/ka/tictactoe/screens/splashScreen/SplashScreenFragment.kt
@@ -1,9 +1,21 @@
 package el.ka.tictactoe.screens.splashScreen
 
 import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import el.ka.tictactoe.R
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenFragment: Fragment(R.layout.splash_screen) {
+    private lateinit var viewModel: SplashScreenViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(SplashScreenViewModel::class.java)
+        viewModel.loadApp()
+    }
+
 }
--- app/src/main/java/el/ka/tictactoe/screens/splashScreen/SplashScreenViewModel.kt
+++ app/src/main/java/el/ka/tictactoe/screens/splashScreen/SplashScreenViewModel.kt
@@ -1,6 +1,21 @@
 package el.ka.tictactoe.screens.splashScreen
 
 import androidx.lifecycle.ViewModel
+import el.ka.tictactoe.R
+import el.ka.tictactoe.general.APP
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 class SplashScreenViewModel: ViewModel() {
+
+    fun loadApp() {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(4000)
+            // Todo (Добавить прогресс-бар загрузки)
+            APP.navController.navigate(R.id.action_splashScreenFragment_to_mainScreenFragment)
+        }
+    }
 }
