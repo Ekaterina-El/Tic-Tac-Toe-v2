8c17226b9f09f8b420dfd8b8957ab47fadcad376
Ekaterina-El <hackerrussia@yandex.ru>
2022-04-01 11:21:45 +0500

    [GameScreen] Added viewModel | Save to viewModel game type | Input game type in layout

--- app/src/main/java/el/ka/tictactoe/screens/gameScreen/GameScreenFragment.kt
+++ app/src/main/java/el/ka/tictactoe/screens/gameScreen/GameScreenFragment.kt
@@ -1,15 +1,48 @@
 package el.ka.tictactoe.screens.gameScreen
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import el.ka.tictactoe.R
+import androidx.lifecycle.ViewModelProvider
+import el.ka.tictactoe.databinding.GameScreenBinding
+import el.ka.tictactoe.general.GameType
 import el.ka.tictactoe.general.GameTypeKey
 
-class GameScreenFragment: Fragment(R.layout.game_screen) {
+class GameScreenFragment : Fragment() {
+    private lateinit var viewModel: GameScreenViewModel
+    private lateinit var binding: GameScreenBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val gameType = requireArguments().getSerializable(GameTypeKey)
-        val a = 1
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        createBinding(inflater)
+        return binding.root
+    }
+
+    private fun createBinding(inflater: LayoutInflater) {
+        createViewModel()
+        initBinding(inflater)
+    }
+
+    private fun initBinding(inflater: LayoutInflater) {
+        binding = GameScreenBinding.inflate(inflater)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.fragmentViewModel = viewModel
+    }
+
+    private fun createViewModel() {
+        viewModel = ViewModelProvider(this).get(GameScreenViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gameType = requireArguments().getSerializable(GameTypeKey) as GameType
+        viewModel.setGameType(gameType)
     }
 }
--- app/src/main/java/el/ka/tictactoe/screens/gameScreen/GameScreenViewModel.kt
+++ app/src/main/java/el/ka/tictactoe/screens/gameScreen/GameScreenViewModel.kt
@@ -1,6 +1,22 @@
 package el.ka.tictactoe.screens.gameScreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import el.ka.tictactoe.general.APP
+import el.ka.tictactoe.general.GameType
 
 class GameScreenViewModel: ViewModel() {
+    private val _gameType = MutableLiveData(GameType.Robot)
+    val gameType: LiveData<GameType>
+        get() = _gameType
+
+
+    fun goBack() {
+        APP.navController.navigateUp()
+    }
+
+    fun setGameType(gameType: GameType) {
+        _gameType.value = gameType
+    }
 }
