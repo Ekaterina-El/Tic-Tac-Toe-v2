429b0fabf43f17f21769b60d7006a2eb42a092d8
Ekaterina-El <hackerrussia@yandex.ru>
2022-04-06 16:08:12 +0500

    [GameScreenFragment] End game when one of player score == 3

--- app/src/main/java/el/ka/tictactoe/ui/screens/gameScreen/GameScreenFragment.kt
+++ app/src/main/java/el/ka/tictactoe/ui/screens/gameScreen/GameScreenFragment.kt
@@ -1,25 +1,36 @@
 package el.ka.tictactoe.ui.screens.gameScreen
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import el.ka.tictactoe.R
 import el.ka.tictactoe.databinding.GameScreenBinding
+import el.ka.tictactoe.general.APP
 import el.ka.tictactoe.general.GAME_BOARD_SIZE
 import el.ka.tictactoe.general.GameType
 import el.ka.tictactoe.general.GAME_TYPE_KEY
 import el.ka.tictactoe.ui.customView.GameBoardEventListener
-import el.ka.tictactoe.ui.customView.GameBoardView
 import el.ka.tictactoe.ui.customView.GameBoardView.Companion.Player
 
 class GameScreenFragment : Fragment(), GameBoardEventListener {
     private lateinit var gameBoardObserver: Observer<Int>
+
     private lateinit var viewModel: GameScreenViewModel
     private lateinit var binding: GameScreenBinding
+
+    private lateinit var endGameDialog: Dialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -77,5 +88,29 @@ class GameScreenFragment : Fragment(), GameBoardEventListener {
             "$player ${this.getString(R.string.won_this_game)}"
         )
         viewModel.updateScoreFor(player)
+
+        if (viewModel.scoreO.value!! == 3) {
+            showEndGameDialog(Player.O)
+        } else if (viewModel.scoreX.value!! == 3) {
+            showEndGameDialog(Player.X)
+        }
+    }
+
+
+    private fun showEndGameDialog(player: Player) {
+        endGameDialog = Dialog(APP)
+        endGameDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        endGameDialog.setContentView(R.layout.game_end_dialog)
+
+        endGameDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        endGameDialog.setCancelable(false)
+        endGameDialog.show()
+
+        endGameDialog.findViewById<TextView>(R.id.whoWin).text =
+            getString(R.string.win, player.toString())
+        endGameDialog.findViewById<LinearLayout>(R.id.dialog_button_quit).setOnClickListener {
+            endGameDialog.dismiss()
+            viewModel.goBack()
+        }
     }
 }
